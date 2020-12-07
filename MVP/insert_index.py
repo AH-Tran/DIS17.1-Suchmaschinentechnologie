a8a9b3925826d3227fb94efe8e051009454af750
AH-Tran <AH_Tran@hotmail.de>
2020-12-07 09:57:21 +0100

    Updated insert_index and gitignore

--- MVP/insert_index.py
+++ MVP/insert_index.py
@@ -1,18 +1,16 @@
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk
 from datetime import datetime
-import os
+import os, uuid
 import pandas as pd
-import csv
-import itertools
-import json
+import csv, itertools, json
 import requests
 
 ## Connect to Elasticsearch
-es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+es = Elasticsearch([{"host": "localhost", "port": 9200}])
 
 ## Generate CSV Dataframe
-csv_df = pd.read_csv('metadata.csv', dtype=str, encoding='utf-8')
+csv_df = pd.read_csv("./MVP/metadata.csv", dtype=str, encoding="utf-8")
 
 ## Get all Data Fields
 column_list = csv_df.columns.to_list()
@@ -22,15 +20,33 @@ for col in column_list:
     csv_df.loc[csv_df[col].isnull(), col] = "None"
 
 ## Transform Dataframe to Dictionary
-documents = csv_df.to_dict(orient='records')
+documents = csv_df.to_dict(orient="records")
 
 ## Check if Index already exists
 if es.indices.exists("covid_index"):
-    es.indices.delete(index="cord")
+    es.indices.delete(index="covid_index")
 
 ## index settings: mapping for publish_time
 settings={
-    "settings": {},
+    "settings": {
+        # just one shard, no replicas for testing
+        "number_of_shards": 1,
+        "number_of_replicas": 0,
+
+        # custom analyzer for covid metadata.csv
+        "analysis": {
+          "analyzer": {
+            "covid_analyzer": {
+              "type": "custom",
+              "tokenizer": "standard",
+              "stopwords": "_english",
+              "filter": ["lowercase",
+                        "asciifolding"
+              ]  
+            }
+          }
+        }
+    },
     "mappings": {
         "properties": {
             "publish_time": {
@@ -44,4 +60,4 @@ settings={
 es.indices.create(index="covid", ignore=400, body=settings)
 
 ## Index Documents
-bulk(es, index='cord', doc_type='_doc', request_timeout=60, raise_on_error=True)
+bulk(es, index="covid_index", doc_type="_doc", request_timeout=60, raise_on_error=True)
