825db7d397e1ddb2885ce955aa8d706cd8e95ae5
AH-Tran <AH_Tran@hotmail.de>
2021-01-17 07:15:10 +0100

    Final_Build uploaded

---
+++ Final_Build/change_csv_dates.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import re
+import pprint
+import datetime
+pd.options.mode.chained_assignment = None
+
+df = pd.read_csv("./MVP/metadata.csv")
+#liste = df["publish_time"]
+#print(liste[0])
+
+
+#print(liste[0:15])
+pattern_date_1 = re.compile(r'^\d\d\d\d-\d\d-\d\d$')
+pattern_date_2 = re.compile(r'^\d\d\d\d$')
+#pattern_date_3 = re.compile(r'^$')
+datums_liste = ["2020-04-01","2020-01-01", "2020-04-01", "2020-04-10", "2020-08-06","2020-07-28","2020-02-25","2020-01-24", "2020-05-18","2020-05-01",
+                "2020-01-01","2020-03-30", "2021-04-01", "2020-03-12", "2021-08-01", "2020-01-28", "2020-01-30","2020-07-28"]
+
+liste_date_1 = []
+liste_date_2 = []
+liste_date_3 = []
+
+for i in range(len(df["publish_time"])):
+    if pattern_date_1.match(str(df["publish_time"][i])):
+        continue
+    elif pattern_date_2.match(str(df["publish_time"][i])):
+        df["publish_time"][i] = df["publish_time"][i].replace(str(df["publish_time"][i]),str(df["publish_time"][i]) + "-01-01" )
+    else:
+        for j in datums_liste:
+            df["publish_time"][i] = j
+
+
+for i in range(len(df["publish_time"])):
+    if pattern_date_1.match(str(df["publish_time"][i])):
+        continue
+    elif pattern_date_2.match(str(df["publish_time"][i])):
+        continue
+    else:
+        liste_date_3.append(df["title"][i])
+
+#print("Gesamte Daten:", len(liste))
+print("Daten mit kompletten Zeitformat:", len(liste_date_1))
+print("Daten (nur Jahreszahl):", len(liste_date_2))
+print("Kein Datum:",len(liste_date_3))
+
+print(liste_date_3)
+#print(len(liste_date_1) +len(liste_date_2)+len(liste_date_3))
+#print(len(liste)-(len(liste_date_1) +len(liste_date_2)+len(liste_date_3)))
+
+df.to_csv("./MVP/metadata_update.csv")
---
+++ Final_Build/index_pipeline.py
@@ -0,0 +1,194 @@
+from elasticsearch import Elasticsearch, helpers
+from elasticsearch.helpers import bulk
+from datetime import datetime
+import os, uuid
+import pandas as pd
+import csv, itertools, json
+import requests
+
+## Connect to Elasticsearch
+es = Elasticsearch([{"host": "localhost", "port": 9200}])
+index_name= "covid_index"
+## Check if Index already exists
+if es.indices.exists(index_name):
+    es.indices.delete(index=index_name)
+
+with open("./Final_Build/synonyms_bearbeitet.txt") as f:
+    content = f.readlines()
+# you may also want to remove whitespace characters like `\n` at the end of each line
+content = [x.strip() for x in content]
+for i in content:
+    i.replace("_", " ")
+#print(content)
+
+#content =["coronavirus, sars-cov-2, sars-cov2, sars cov 2,covid19, covid-19, covid 19, covid" ] 
+## index settings: mapping for publish_time
+doc_settings = {
+    "settings": {
+        # just one shard, no replicas for testing
+        "number_of_shards": 1,
+        "number_of_replicas": 0,
+
+        # custom analyzer for covid metadata.csv
+        "analysis": {
+            "filter": {
+                "synonym": {
+                    "type": "synonym",
+                    "lenient": True,
+                    "synonyms": content
+                },
+                "acronym": {
+                    "type": "word_delimiter",
+                    "catenate_all": True,
+                    "generate_word_parts": False,
+                    "generate_number_parts": False,
+                    "preserve_original": True
+                },
+                "my_snow":{
+                    "type": "snowball",
+                    "language": "English"
+                },
+                "english_stemmer" : {
+                    "type" : "stemmer",
+                    "language" : "english"
+                },
+              "keyword_list": {
+                "type": "keyword_marker",# plural or singular?
+                "keywords": ["Covid-19", "SARS-COV-2", "SARS-COV", "2019-nCOV"
+                         "United States", "United Kingdom", "Hong Kong", "United Arab Emirates",
+                         "non-social", "African-American",
+                          "mRNA ", "ACE inhibitor", "enzyme inhibitors", "blood type", "Angiotensin-converting", "clinical signs", "super spreaders", "hand sanitizer", "alcohol sanitizer", ""]#
+                 },
+                "ngram_filter": {
+                    "type":     "ngram",
+                    "min_gram": 3,
+                    "max_gram": 3,
+                    "token_chars": [
+                        "letter",
+                        "digit",
+                        "punctuation",
+                        "symbol"
+                    ]
+                }
+            },# FILTER END
+            "char_filter": {
+                "covid_char_filter": {
+                "type": "mapping",
+                            "mappings": [
+                                "$=> dollar",
+                                "€=> euro",
+                                "£=> pound",
+                                "%=> percentage"
+                                ]
+                }
+             },
+             #},# INDEX END
+            "analyzer": {
+                "covid_analyzer": {
+                    "type": "custom",
+                    "tokenizer": "standard",
+                    "stopwords": "_english",
+                    "filter": ["synonym",
+                                "acronym",
+                               "lowercase",
+                               "stop",
+                               "asciifolding",
+                               "my_snow",
+                               "keyword_list"],
+                    "char_filter": ["covid_char_filter"]
+                },
+                "ngram_analyzer": {
+                    "type":      "custom",
+                    "tokenizer": "standard",
+                    "filter":   [
+                        "lowercase",
+                        "ngram_filter"
+                    ]
+                },
+                "query_analyzer":{
+                    "type": "custom",
+                    "tokenizer": "standard",
+                    "stopwords": "_english",
+                    "filter": ["lowercase",
+                               "stop",
+                               "asciifolding",
+                               "my_snow",
+                               "keyword_list"],
+                    "char_filter": ["covid_char_filter"]
+                    }
+                }# ANALYZER END
+        },# ANALYSIS END
+    #SIMILARITY MODULE
+    "similarity" : {
+               "BM25_similarity": {
+                    "type": "BM25",
+                    "k1": "1.2", #the higher, the more relevant TF
+                    "b": "0.75" #the higher, the higher the impact of document length
+                  },
+                  "DFR_similarity": {
+                    "type": "DFR",
+                    "basic_model": "g",
+                    "after_effect": "l",
+                    "normalization": "h2"
+                  },
+                  "LMJelinekMercer_short": {
+                    "type": "LMJelinekMercer",
+                    "lambda": "0.1"
+                  },
+                 "LMJelinekMercer_long": {
+                    "type": "LMJelinekMercer",
+                    "lambda": "0.7"
+                  },
+                "TFIDF": {
+                    "type": "scripted",
+                    "script": {
+                    "source": "double tf = Math.sqrt(doc.freq); double idf = Math.log((field.docCount+1.0)/(term.docFreq+1.0)) + 1.0; double norm = 1/Math.sqrt(doc.length); return query.boost * tf * idf * norm;"
+                    }
+                }
+        }#SIMILARITY END
+    },# SETTINGS END
+
+    "mappings": {
+        "properties": {
+            "title": { 
+                "type": "text",
+                "fields": {
+                    "analysis": { 
+                        "type":     "text",
+                        "analyzer": "covid_analyzer",
+                        "similarity": "BM25_similarity"
+                    },
+                    "ngram": { 
+                        "type":     "text",
+                        "analyzer": "ngram_analyzer",
+                        "similarity": "BM25_similarity"
+                    },
+                    "keyword": { 
+                        "type":     "keyword"
+                    }
+                }
+            },
+            "abstract": {
+                "type": "text",
+                "analyzer": "covid_analyzer",
+                "similarity": "DFR_similarity"
+            },
+            "publish_time": {
+                "type": "date"
+            },
+            "query": {
+                "type": "text",
+                "analyzer": "query_analyzer"
+            }
+        } # PROPERTIES END
+    } # MAPPINGS END
+}# DOC_SETTINGS END
+
+## Create Index
+es.indices.create(index=index_name, ignore=400, body=doc_settings)
+es.indices.analyze(index=index_name, ignore=400, body=doc_settings)
+
+## Index Documents
+with open("./Final_Build/metadata.csv", "r", encoding="utf8") as f:
+    reader = csv.DictReader(f)
+    helpers.bulk(es, reader, index=index_name,raise_on_error=False, stats_only=False)
