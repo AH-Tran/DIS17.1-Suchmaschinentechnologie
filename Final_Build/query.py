32c0a82276a0d3502664e099d93ba5e14a628d88
AH-Tran <AH_Tran@hotmail.de>
2021-01-17 10:01:08 +0100

    Update query.py

--- Final_Build/query.py
+++ Final_Build/query.py
@@ -3,19 +3,65 @@ import xml.etree.ElementTree as ET
 import json
 
 path = 'topics-rnd5_covid-complete.xml'
-collection_name = "default_index"
+collection_name = "covid_index"
+
 
 def import_querys(path):
     tree = ET.parse(path)
     root = tree.getroot()
     query_list = []
-    for topic in root:
+    narrative_list = []
+    question_list = []
+    for topic in root:    
         for querys in topic.findall("query"):
             query_list.append(querys.text)
+        #for narratives in topic.findall("narrative"):
+         #   narrative_list.append(narratives.text)
+        #for questions in topic.findall("question"):
+            #  question_list.append(questions.text)
 
     formatted_query_list = []
     for i in range(50):
-        query = {"query": {"multi_match": {"query": query_list[i], "fields": ["title", "abstract"]}}}
+
+
+        query = {
+            "query": {
+                "bool": {
+                    "must": {
+                        "bool": {
+                            "should": {
+                                "multi_match": {
+                                    "query": query_list[i],
+                                    "analyzer": "query_analyzer",
+                                    "fields": "title"},
+                                "multi_match": {
+                                    "query": query_list[i],
+                                    "analyzer": "query_analyzer",
+                                    "fields": "abstract"},
+                            }}},
+                    "should": [
+                        {
+                            "range": {
+                                "publish_time": {
+                                    "boost": 1.2,
+                                    "gte": "2020-01-01"
+                                }
+                            }
+                        },
+                        {
+                            "range": {
+                                "publish_time": {
+                                    "lt": "2020-01-01",
+                                    "boost": 0.8
+                                }
+                            }
+                        }
+                    ]
+
+                }
+            }
+        }
+        
         formatted_query_list.append(query)
 
     return formatted_query_list
@@ -24,27 +70,31 @@ def import_querys(path):
 liste = import_querys(path)
 
 
-def search(collection_index, formatted_query_list):
+# print(liste[0])
 
+def search(collection_index, formatted_query_list):
     liste_strings = []
-    #myfile = open("results.txt", 'w')
+    # myfile = open("results.txt", 'w')
     for i in range(len(formatted_query_list)):
         id_list = []
         elastic_client = Elasticsearch()
-        response = elastic_client.search(index=collection_index, body=json.dumps(formatted_query_list[i]), size=1000    )
+        response = elastic_client.search(index=collection_index, body=json.dumps(formatted_query_list[i]),
+                                         size=1000, request_timeout=(100))
 
-        #print("Num\titeration\t\tTitle\t\tRelevance Score")
+        # print("Num\titeration\t\tTitle\t\tRelevance Score")
         for idx, hit in enumerate(response['hits']['hits']):
             if str(hit['_source']['cord_uid']) not in id_list:
                 id_list.append(hit['_source']['cord_uid'])
-                string = str(i+1) + " " + "Q0" + " " + str(hit['_source']['cord_uid']) + " " + str(idx) + " " + str(hit['_score']) + " Standard"
+                string = str(i + 1) + " " + "Q0" + " " + str(hit['_source']['cord_uid']) + " " + str(idx) + " " + str(
+                    hit['_score']) + " Standard"
                 liste_strings.append(string)
             else:
                 continue
-    myfile = open("results.txt", 'w', encoding="utf-8", newline='\n')
+    myfile = open("results_update2.txt", 'w', encoding="utf-8", newline='\n')
 
     for i in range(len(liste_strings)):
         myfile.write(liste_strings[i] + "\n")
     myfile.close()
+
 
 search(collection_name, liste)
